4f02b98e699a4f58c932a145ab1d1e499569150d
Luke <luke.e.philips@gmail.com>
2016-12-09 08:00:08 -0800

    Barber Class complete for CRUD

---
+++ lib/barbers.rb
@@ -0,0 +1,33 @@
+class Barber
+  attr_reader(:name, :specialty, :id)
+  def initialize(attributes)
+    @name = attributes[:name]
+    @specialty = attributes[:specialty]
+    @id = attributes[:id]
+  end
+  def save
+    result = DB.exec("INSERT INTO barbers (name) VALUES ('#{name}') RETURNING id;")
+    @id = result[0]['id'].to_i
+  end
+  def ==(other)
+    (self.name == other.name)
+  end
+  def delete
+    DB.exec("DELETE FROM barbers WHERE id = #{self.id()};")
+  end
+  def update(key, value)
+    DB.exec("UPDATE barbers SET #{key} = '#{value}' WHERE id = #{self.id};")
+  end
+
+  def self.all
+    barbers = []
+    returned_barbers = DB.exec("SELECT * FROM barbers;")
+    returned_barbers.each do |a|
+      name = a['name']
+      specialty = a['specialty']
+      id = a['id'].to_i
+      barbers.push(Barber.new(:name => name, :specialty => specialty, :id => id))
+    end
+    barbers
+  end
+end
---
+++ spec/barbers_spec.rb
@@ -0,0 +1,42 @@
+require "spec_helper"
+
+describe Barber do
+  before do
+    @barber1 = Barber.new(:name => 'JaRutherford', :specialty => 'braids', :id => nil)
+  end
+
+  describe ".all" do
+    it 'returns an empty array if no barbers exist' do
+      expect(Barber.all).to eq([])
+    end
+  end
+
+  describe '#save' do
+    it 'returns a barber' do
+      @barber1.save
+      expect(Barber.all).to eq([@barber1])
+    end
+  end
+  describe '#==' do
+    it "returns true when two objects attributes are the same" do
+      barber  = Barber.new(:id => nil, :specialty => 'fades', :name => "Darius")
+      expect(barber).to eq(barber)
+    end
+  end
+  describe '#delete' do
+    it "returns an empty array when only item is deleted" do
+      barber  = Barber.new(:id => nil, :specialty => 'fades', :name => "Darius")
+      barber.save
+      barber.delete
+      expect(Barber.all).to eq([])
+    end
+  end
+  describe '#update' do
+    it "updates a barbers specialty" do
+      barber  = Barber.new(:id => nil, :specialty => 'fades', :name => "Darius")
+      barber.save
+      barber.update('specialty', 'shave')
+      expect(Barber.all[0].specialty).to eq('shave')
+    end
+  end
+end
